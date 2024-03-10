4783a86b5d9a4e13d7dc295f00501297451326d5
tassh571 <tassh571@postbox.in.th>
2024-03-10 23:03:32 +0700

    Update

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="Quenser",
-    version="0.0.1",
+    version="0.0.2",
     author="Tassana Khrueawan",
     author_email="tassana.khr@gmail.com",
-    description="Hello world Testing Library for Python",
+    description="A custom library for Robot Framework that allows you to say hello",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/tassh571/Quenser.git",
     packages=find_packages(),
+    install_requires=[
+        'robotframework>=3.0', 
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
