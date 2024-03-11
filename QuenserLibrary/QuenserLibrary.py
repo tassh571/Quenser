9a94dbf5a2c3c510cd8a243e46f277b7c6d08081
tassh571 <tassh571@postbox.in.th>
2024-03-11 13:21:25 +0700

    Update Code Version 0.0.10

--- QuenserLibrary/QuenserLibrary.py
+++ QuenserLibrary/QuenserLibrary.py
@@ -2,8 +2,14 @@
 #QuenserLibrary.py
 
 from robot.api.deco import keyword
+from .ScreenshotKeywords import ScreenshotKeywords
 
-class QuenserLibrary:
+class QuenserLibrary(ScreenshotKeywords):
     def PrintTEST(self):
-        """ พิมพ์ข้อความ 'Hello, world!' ลงในคอนโซล """
+        """ พิมพ์ข้อความ 'Hello, world!' ลงในคอนโซล แบบไม่ได้แอดคีย์ """
         print("Hello, world!")
+
+    @keyword("Hello world")
+    def Hello(self):
+        """ พิมพ์ข้อความ 'Hello, world!' ลงในคอนโซล แบบแอดคีย์ """
+        print("Hello, world! Test keyword='Hello, world!'")
