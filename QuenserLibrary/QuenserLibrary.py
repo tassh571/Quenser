af9e74cd182262eb01bd59d60318c9641092b233
tassh571 <tassh571@postbox.in.th>
2024-03-12 16:43:14 +0700

    Update Code V1.6

--- QuenserLibrary/QuenserLibrary.py
+++ QuenserLibrary/QuenserLibrary.py
@@ -4,9 +4,24 @@
 from robot.api.deco import keyword
 from .TineLibrary import TineLibrary
 from .asdasd import asdasd
+from robot.libraries.BuiltIn import BuiltIn
 
 
 class QuenserLibrary(TineLibrary, asdasd):
+
+    def __init__(self):
+        self._bi = BuiltIn()
+
+    def t_quit_app(self):
+        """ปิดแอพปัจจุบันและปิดเซสชัน"""
+        driver = self._current_application()
+        driver.quit()
+
+    def _current_application(self):
+        """คืนค่าอินสแตนซ์ของแอปพลิเคชันปัจจุบัน"""
+        return self._bi.get_library_instance('AppiumFlutterLibrary')._current_application()
+
+    
     def PrintTEST(self):
         """ พิมพ์ข้อความ 'Hello, world!' ลงในคอนโซล แบบไม่ได้แอดคีย์ """
         print("Hello, world! เทสภาษาไทยfdsafadsfsadfdsafdsa")
